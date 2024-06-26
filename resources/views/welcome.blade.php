46b439d6ab69a5af5d4e3ac716cf9016bcd4b9aa
morine <fold.it.fold.it@gmail.com>
2024-06-26 14:08:49 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -910,7 +910,7 @@
                 <nav class="-mx-3 flex flex-1 justify-center">
                     @auth
                     <a href="{{ url('/dashboard') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                        Dashboard
+                        管理画面へ
                     </a>
                     @else
                     <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
